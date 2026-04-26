d78b5f7e1afea07e4e123e6c520b462b28a4cc49
Dengke <dearwangsr@gmail.com>
2026-04-26 23:31:51 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'liquor-pwa-v1';
+const urlsToCache = [
+  './',
+  './白酒价格记录.html',
+  './manifest.json',
+  './icon-192.svg',
+  './icon-512.svg'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
